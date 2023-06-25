56ce2a48891217151a249c712931041696489c38
CJYKK <2159913864@qq.com>
2023-06-25 17:54:11 +0800

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import logo from './AntiTUSITUProject_Icon.png';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  banner: {
+    dismissible: false,
+    text: (
+      <a href="https://github.com/AntiTUSITUProject" target="_blank">
+        🔔 该网站仍在建设中。您可以在GitHub上给我们提供帮助，任何帮助都会使我们变得更好！（点击前往）
+      </a>
+    )
+  },
+  search: {
+    placeholder: "搜索……",
+    error: "无法加载搜索系统"
+  },
+  head: (
+    <link rel="icon" href={logo.src} type="image/x-icon" />
+  ),
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – 反吐司兔计划'
+    }
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/AntiTUSITUProject',
   },
-  chat: {
-    link: 'https://discord.com',
+  feedback: {
+    content: null
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  editLink: {
+    component: null
+  },
+  toc: {
+    title: <span>目录</span>,
+    extraContent: <span>反吐司兔计划<br />希望能够帮到你 ❤</span>
+  },
+  gitTimestamp: null,
+  logo: <><img src={logo.src} alt="logo" style={{ width: '50px', height: '50px' }} /><span>⁣⁣⁣⁣⁣⁣⁣⁣ ⁣⁣⁣⁣ ⁣⁣⁣⁣ ⁣反吐司兔计划</span></>,
+  docsRepositoryBase: 'https://github.com/AntiTUSITUProject/blob/main',
   footer: {
-    text: 'Nextra Docs Template',
-  },
+    text: (
+      <span>
+        版权所有 © {new Date().getFullYear()} { }
+        <a href="https://www.tusitu123.top" target="_blank">
+          反吐司兔计划 (Anti TUSITU Project)
+        </a>
+      </span>
+    )
+  }
 }
 
 export default config
